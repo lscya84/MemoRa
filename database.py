e34a22fb58ad55d7379d50a50f2321af82e1b26d
lscya84 <twitch@nate.com>
2026-02-07 17:02:59 +0900

    requirements.txt openai 추가

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+import os
 from sqlalchemy import create_engine, Column, Integer, String, Float, Text, JSON, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
